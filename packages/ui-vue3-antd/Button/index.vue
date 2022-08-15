32df97bed3a68b68a6c93016d8544012e03d88ee
caesar <caesar.luo@silksoftware.com>
2022-08-15 11:19:03 +0800

    chore: antd ui

--- packages/ui-vue3-antd/Button/index.vue
+++ packages/ui-vue3-antd/Button/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Button>
+  <Button v-bind="$props">
     <slot />
   </Button>
 </template>
@@ -8,8 +8,41 @@
 import { Button } from 'ant-design-vue'
 
 export default {
+  name: 'VButton',
   components: {
     Button
-  }
+  },
+  props: {
+    block: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    href: {
+      type: String
+    },
+    htmlType: {
+      type: String,
+      default: 'button'
+    },
+    target: {
+      type: String,
+      default: '_self',
+      validator: (value) => {
+        return ['_blank', '_self', '_parent', '_top'].includes(value)
+      }
+    },
+    type: {
+      type: String,
+      default: 'default',
+       validator: (value) => {
+        return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(value)
+      }
+    }
+  },
+  emits: ['click']
 }
 </script>
