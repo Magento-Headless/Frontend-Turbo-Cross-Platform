f6c12a3f2fcface6eea510cd6f5dba0cc718f696
caesar <454451758@qq.com>
2022-08-17 17:15:19 +0800

    chore: vite material

---
+++ apps/storefront-vite-react-material/vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig, loadEnv } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import graphql from '@rollup/plugin-graphql'
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), 'REACT_')}
+  
+  return defineConfig({
+    envPrefix: 'REACT_',
+    plugins: [
+      react({
+        babel: {
+          babelrc: true
+        },
+        jsxRuntime: 'classic'
+      }),
+      graphql()
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '@components': path.resolve(__dirname, './components'),
+        '@graphql': path.resolve(__dirname, './graphql'),
+        '@hooks': path.resolve(__dirname, './hooks')
+      }
+    },
+    build: {
+      target: 'esnext',
+      outDir: 'dist',
+      minify: 'esbuild',
+      chunkSizeWarningLimit: 150
+    },
+    server: {
+      host: 'localhost',
+      port: 3000,
+      https: false,
+      open: true,
+      cors: true,
+      hmr: true
+    }
+  })
+}
